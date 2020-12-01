0a9cff21ef04f9367ecd30b1d8b7a4ca22f2cdcc
nguyengiang <giangnt623@wru.vn>
2020-12-01 23:59:24 +0700

    dùng bốttrap chia lưới

--- index.php
+++ index.php
@@ -3,10 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>bootstran</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="style.css">
+    <title>bootstrap</title>
 </head>
 <body>
-    
+       <div class="container" style="background-color:orange;">
+           <div class="row">
+               <div class="col-md-4 col-sm-6">
+                    <div class="khoi"></div>
+                    
+               </div>
+               <div class="col-md-8 col-sm-6">
+               <div class="khoi"></div>
+               </div>
+           </div>
+       </div>
+       <br>
+       <div class="container-fluid" style="background-color:orange;">
+           <div class="row">
+               <div class="col-md-4">
+                    <div class="khoi"></div>
+                    
+               </div>
+               <div class="col-md-8">
+               <div class="khoi"></div>
+               </div>
+           </div>
+       </div>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
